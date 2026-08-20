86c07d4c929f1041225533ec662fad82df746742
Maik-Oliver towet <olli@iMac-von-Maik-Oliver.local>
2026-08-20 15:03:36 +0200

    Revert "Harden mail transport configuration"
    This reverts commit 05389bd373d169745c96243d4db4b35272d54bfc.

--- app/Services/TenantMailConfigurator.php
+++ app/Services/TenantMailConfigurator.php
@@ -13,29 +13,13 @@ class TenantMailConfigurator
             return;
         }
 
-        $port = filled($tenant->mail_port)
-            ? (int) $tenant->mail_port
-            : (int) config('mail.mailers.smtp.port', 587);
-
-        $encryption = filled($tenant->mail_encryption)
-            ? $tenant->mail_encryption
-            : config('mail.mailers.smtp.encryption');
-
-        if (blank($encryption)) {
-            $encryption = match ($port) {
-                465 => 'ssl',
-                587 => 'tls',
-                default => null,
-            };
-        }
-
         Config::set('mail.default', 'smtp');
         Config::set('mail.mailers.smtp.transport', $tenant->mail_mailer ?? 'smtp');
         Config::set('mail.mailers.smtp.host', $tenant->mail_host);
-        Config::set('mail.mailers.smtp.port', $port);
+        Config::set('mail.mailers.smtp.port', $tenant->mail_port);
         Config::set('mail.mailers.smtp.username', $tenant->mail_username);
         Config::set('mail.mailers.smtp.password', $tenant->mail_password);
-        Config::set('mail.mailers.smtp.encryption', $encryption);
+        Config::set('mail.mailers.smtp.encryption', $tenant->mail_encryption);
         Config::set('mail.from.address', $tenant->mail_from_address ?? 'noreply@clubano.de');
         Config::set('mail.from.name', $tenant->mail_from_name ?? 'Clubano');
     }
